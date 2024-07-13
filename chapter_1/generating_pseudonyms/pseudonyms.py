db90740d302c49d06a46374c9b4d559b898a3988
Miguel-Angel Sanchez Ndoye <miguel.ngl.sanchez@gmail.com>
2024-07-13 22:02:17 +0200

    implemented the pseudonyms generator

---
+++ chapter_1/generating_pseudonyms/pseudonyms.py
@@ -0,0 +1,49 @@
+import sys, random
+
+WELCOME_MESSAGE = "Welcome to the Psych 'Sidekick Name Picker.'\n" + \
+                  "A name just like Sean would pick for Gus:\n"
+
+first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+         "Winston 'Jazz Hands'", 'Worms')
+
+last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+        'Woolysocks')
+
+names = (first, last)
+
+def main():
+    print(WELCOME_MESSAGE)
+    while True :
+        user_choice = input('generate new pseudonym? [y/n]: ')
+        if user_choice.lower() in ('n', 'no'):
+            break
+        generated_name = ' '.join([random.choice(names_tuple) for names_tuple in names])
+        print('Generated name : {}\n'.format(generated_name), file=sys.stderr)
+
+if __name__ == '__main__' :
+    main()
